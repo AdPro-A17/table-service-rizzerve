93427cbd86637593e721045df1e5305cc7376817
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-09 15:16:01 +0700

    feat: initialize

---
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/TableServiceRizzerveApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableServiceRizzerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableServiceRizzerveApplication.class, args);
+	}
+
+}
