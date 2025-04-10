1ef5c6b0f4707a8882b0ed09f9102929125a1a8e
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 01:04:21 +0700

    ci: Added SonarQube CSUI workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
 	java
 	jacoco
+	id("org.sonarqube") version "6.0.1.5171"
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "table-service-rizzerve")
+		property("sonar.projectName", "table-service-rizzerve")
+	}
 }
 
 group = "id.ac.ui.cs.advprog"
