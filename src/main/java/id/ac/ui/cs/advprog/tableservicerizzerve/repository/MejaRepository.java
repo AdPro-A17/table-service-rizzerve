dfd9ee97588144d7a216eeb319c071d88ac39a85
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 19:14:28 +0700

    [RED] Add skeleton for MejaRepository findAll method

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
@@ -3,16 +3,22 @@ package id.ac.ui.cs.advprog.tableservicerizzerve.repository;
 import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.List;
 
 @Repository
 public class MejaRepository {
-    private final Map<UUID, Meja> mejaStorage = new ConcurrentHashMap<>();
+
+    private final Map<UUID, Meja> mejaStorage = new HashMap<>();
 
     public Meja save(Meja meja) {
         mejaStorage.put(meja.getId(), meja);
         return meja;
+    }
+
+    public List<Meja> findAll() {
+        return null;
     }
 }
