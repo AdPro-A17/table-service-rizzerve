d42c94c821d5e3ba5677d58ea0e101298d6cd02a
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 18:23:33 +0700

    [GREEN] Implement MejaEventListener for MejaCreatedEvent

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/observer/MejaEventListener.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/observer/MejaEventListener.java
@@ -1,10 +1,13 @@
 package id.ac.ui.cs.advprog.tableservicerizzerve.observer;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MejaEventListener {
 
+    @EventListener
     public void handleMejaCreatedEvent(MejaCreatedEvent event) {
+        System.out.println("Event received - Meja created with nomorMeja: " + event.getMeja().getNomorMeja());
     }
 }
