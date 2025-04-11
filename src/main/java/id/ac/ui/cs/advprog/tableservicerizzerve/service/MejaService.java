bbeb22b9be664d48edfae42f23010e2f196cc010
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 19:22:06 +0700

    [RED] Add skeleton for MejaService findAllMeja method

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/service/MejaService.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/service/MejaService.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.tableservicerizzerve.service;
 
 import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
 
+import java.util.List;
+
 public interface MejaService {
     Meja createMeja(int nomorMeja, String status);
+    List<Meja> findAllMeja();
 }
