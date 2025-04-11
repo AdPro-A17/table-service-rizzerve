6b94da6dc947cfb564993a9e5947cd5cddb57d0a
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 19:14:58 +0700

    [GREEN] Implement MejaRepository findAll method

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.List;
+import java.util.ArrayList;
 
 @Repository
 public class MejaRepository {
@@ -19,6 +20,6 @@ public class MejaRepository {
     }
 
     public List<Meja> findAll() {
-        return null;
+        return new ArrayList<>(mejaStorage.values());
     }
 }
