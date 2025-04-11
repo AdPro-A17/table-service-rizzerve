aaf833343311ecb2fe94ca81a23071d7d190b877
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 16:33:28 +0700

    [RED] Add skeleton for MejaController

---
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/controller/MejaController.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve.controller;
+
+import id.ac.ui.cs.advprog.tableservicerizzerve.service.MejaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/meja")
+@RequiredArgsConstructor
+public class MejaController {
+
+    private final MejaService mejaService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createMeja(@RequestBody CreateMejaRequest request) {
+        return null;
+    }
+}
