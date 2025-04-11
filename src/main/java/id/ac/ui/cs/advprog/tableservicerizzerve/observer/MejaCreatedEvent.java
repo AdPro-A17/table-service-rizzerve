d7be4d6ef03559cb495a70900e3aa59b2779c880
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 15:37:33 +0700

    [GREEN] Implement MejaCreatedEvent

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/observer/MejaCreatedEvent.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/observer/MejaCreatedEvent.java
@@ -1,15 +1,16 @@
 package id.ac.ui.cs.advprog.tableservicerizzerve.observer;
 
 import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
+@Getter
 public class MejaCreatedEvent extends ApplicationEvent {
+
+    private final Meja meja;
 
     public MejaCreatedEvent(Object source, Meja meja) {
         super(source);
-    }
-
-    public Meja getMeja() {
-        return null;
+        this.meja = meja;
     }
 }
