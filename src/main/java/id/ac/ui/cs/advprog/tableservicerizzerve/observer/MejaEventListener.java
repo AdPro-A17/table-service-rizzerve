a54c14258a2203b2be5e69136d5aa9585f81ac20
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 18:21:42 +0700

    [RED] Add skeleton for MejaEventListener

---
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/observer/MejaEventListener.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve.observer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MejaEventListener {
+
+    public void handleMejaCreatedEvent(MejaCreatedEvent event) {
+    }
+}
