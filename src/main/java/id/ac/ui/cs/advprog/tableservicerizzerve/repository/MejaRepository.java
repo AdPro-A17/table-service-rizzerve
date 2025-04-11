39192879ae49bbbda40d39fdcaef90f1b143c1af
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 15:18:22 +0700

    [RED] Add skeleton for MejaRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve.repository;
+
+import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
+
+public class MejaRepository {
+    public Meja save(Meja meja) {
+        return null;
+    }
+}
