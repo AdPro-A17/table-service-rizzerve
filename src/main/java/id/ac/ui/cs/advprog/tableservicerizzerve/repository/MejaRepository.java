693ce8f69415c0b306d5617ee40f04edbce6b4e6
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 15:44:06 +0700

    [REFACTOR] Add repository stereotype

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepository.java
@@ -1,11 +1,13 @@
 package id.ac.ui.cs.advprog.tableservicerizzerve.repository;
 
 import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
+import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class MejaRepository {
     private final Map<UUID, Meja> mejaStorage = new ConcurrentHashMap<>();
 
