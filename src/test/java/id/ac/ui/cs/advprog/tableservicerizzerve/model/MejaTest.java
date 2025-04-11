2716f0d67e489eaa038309b31ac8f7193ebb49bb
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 14:50:18 +0700

    [RED] Add tests for Meja model

---
+++ src/test/java/id/ac/ui/cs/advprog/tableservicerizzerve/model/MejaTest.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MejaTest {
+
+    private Meja meja;
+
+    @BeforeEach
+    void setUp() {
+        this.meja = new Meja(10, "tersedia");
+    }
+
+    @Test
+    void testGetId_NotNull() {
+        assertNotNull(this.meja.getId());
+    }
+
+    @Test
+    void testGetNomorMeja() {
+        assertEquals(10, this.meja.getNomorMeja());
+    }
+
+    @Test
+    void testGetStatus() {
+        assertEquals("tersedia", this.meja.getStatus());
+    }
+}
