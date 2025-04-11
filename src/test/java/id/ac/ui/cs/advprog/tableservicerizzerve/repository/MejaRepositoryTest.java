56ec922ed0f8a34a59b71a441c7e6a179ca1e659
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 19:08:19 +0700

    [RED] Add test for MejaRepository findAll method

--- src/test/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/tableservicerizzerve/repository/MejaRepositoryTest.java
@@ -4,6 +4,8 @@ import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MejaRepositoryTest {
@@ -24,5 +26,20 @@ class MejaRepositoryTest {
         assertEquals(meja.getId(), savedMeja.getId());
         assertEquals(5, savedMeja.getNomorMeja());
         assertEquals("TERSEDIA", savedMeja.getStatus());
+    }
+
+    @Test
+    void testFindAllMeja() {
+        Meja meja1 = new Meja(1, "TERSEDIA");
+        Meja meja2 = new Meja(2, "TERSEDIA");
+
+        mejaRepository.save(meja1);
+        mejaRepository.save(meja2);
+
+        List<Meja> result = mejaRepository.findAll();
+
+        assertEquals(2, result.size());
+        assertTrue(result.contains(meja1));
+        assertTrue(result.contains(meja2));
     }
 }
