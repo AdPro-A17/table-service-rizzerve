0a62cccfce173decdb245a3b93bd08312efe8171
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-04-11 19:32:56 +0700

    [GREEN] Add GetAllMejaResponse response class

---
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/controller/GetAllMejaResponse.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.tableservicerizzerve.controller;
+
+import id.ac.ui.cs.advprog.tableservicerizzerve.model.Meja;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetAllMejaResponse {
+    private String status;
+    private List<Meja> data;
+}
