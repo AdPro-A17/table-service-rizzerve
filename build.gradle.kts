4a9cd6887d6f5360c98a207b3eda8127f93a39dd
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-05-02 12:29:44 +0700

    [REFACTOR] Add Spring Data JPA, H2 and JWT deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,14 @@ val junitJupiterVersion = "5.9.1"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	runtimeOnly("com.h2database:h2")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
