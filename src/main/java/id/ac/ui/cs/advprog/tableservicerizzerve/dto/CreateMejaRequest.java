07ee0a9337dd3163caf8bf7f402d77e2c6678220
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-05-09 15:51:48 +0700

    [REFACTOR] add not blank validation for dto

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/dto/CreateMejaRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/dto/CreateMejaRequest.java
@@ -2,10 +2,12 @@ package id.ac.ui.cs.advprog.tableservicerizzerve.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class CreateMejaRequest {
+    @NotBlank(message = "Nomor meja cannot be emoty")
     private int nomorMeja;
     private String status;
 }
--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/dto/UpdateMejaRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/dto/UpdateMejaRequest.java
@@ -2,10 +2,12 @@ package id.ac.ui.cs.advprog.tableservicerizzerve.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class UpdateMejaRequest {
+    @NotBlank(message = "Nomor meja cannot be empty")
     private int nomorMeja;
     private String status;
 }
