88a41de797dec2f4340fc88587fd85f6b70aa2b2
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-05-25 17:24:07 +0700

    [REFACTOR] fix maintanability issues related to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,16 @@ repositories {
 	mavenCentral()
 }
 
+val postgresqlVersion = "42.7.3"
+val dotenvJavaVersion = "2.3.2"
+val jjwtVersion = "0.12.5"
+val h2Version = "2.2.224"
+val testcontainersVersion = "1.19.7"
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
+val micrometerPrometheusVersion = "1.12.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -44,30 +50,34 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
-	runtimeOnly("org.postgresql:postgresql:42.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-	implementation("io.github.cdimascio:dotenv-java:2.3.2")
+	runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
 
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-	implementation("io.github.cdimascio:dotenv-java:2.3.2")
+	implementation("io.github.cdimascio:dotenv-java:$dotenvJavaVersion")
+
+	implementation("io.micrometer:micrometer-registry-prometheus:$micrometerPrometheusVersion")
+
+	implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
 
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	testImplementation("com.h2database:h2")
+	testImplementation("com.h2database:h2:$h2Version")
+	testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+	testImplementation("org.testcontainers:rabbitmq:$testcontainersVersion")
+
 	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
 	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
 	testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
-
-	testImplementation("org.testcontainers:junit-jupiter:1.19.7")
-	testImplementation("org.testcontainers:rabbitmq:1.19.7")
 }
 
 tasks.register<Test>("unitTest") {
