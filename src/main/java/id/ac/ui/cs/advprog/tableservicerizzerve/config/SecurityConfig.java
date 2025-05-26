f41d19f5c5e97782af972d04e6412278ba1ee13e
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-05-27 00:18:59 +0700

    [REFACTOR] update for deployment

--- src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/tableservicerizzerve/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("${FRONTEND_URL}"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList(
